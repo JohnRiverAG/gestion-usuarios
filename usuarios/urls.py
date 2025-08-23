62f73b78ee5115c0cfe54b1e2affe3e968cafac4
John Arango <johnunivalle@gmail.com>
2025-08-23 02:44:43 +0200

    Cambio Dashboard

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('registro/', views.registro, name='registro'),
-    path('', views.iniciar_sesion, name='login'),
+    path('login/', views.iniciar_sesion, name='login'),
     path('logout/', views.cerrar_sesion, name='logout'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
     path('lista/', views.lista_usuarios, name='lista'),
     path('editar/<int:usuario_id>/', views.editar_usuario, name='editar'),
     path('eliminar/<int:usuario_id>/', views.eliminar_usuario, name='eliminar'),
